6ff961b87c3a8e4492cc6c2c245dcb365d768f67
Miranda-03 <martin.miranda.2004@gmail.com>
2026-09-14 17:48:20 -0300

    Modelos (+500)

--- TGC.MonoGame.TP/ModelsInScene/ModelInScene.cs
+++ TGC.MonoGame.TP/ModelsInScene/ModelInScene.cs
@@ -55,6 +55,9 @@ namespace TGC.MonoGame.TP.ModelsInScene
                     foreach (var meshPart in mesh.MeshParts)
                     {
                         var basicEffect = (BasicEffect)meshPart.Effect;
+                        Console.WriteLine(
+                        $"{GetType().Name} - Texture = {basicEffect.Texture}"
+                            );
                         GetTextures().Add(basicEffect.Texture);
                         meshPart.Effect = _renderer._effect;
                     }
